424ba97f9a3f41604a2d52fb1ac9a892d90b526e
manisha2710 <manishamanu666@gmail.com>
2025-12-01 23:35:49 +0530

    Assignment1

---
+++ Task 1.py
@@ -0,0 +1,14 @@
+""" Basic Mathematical Operations
+Taking  two numbers as Input from user"""
+
+
+Number1 = int(input("Enter the first number: " ))
+Number2 = int(input("Enter the second number: " ))
+Addition = (Number1 + Number2)
+Subtraction = (Number1 - Number2)
+Multiplication = (Number1 * Number2)
+Division = (Number1 / Number2)
+print("Addition: ", Addition)
+print("Subtraction: ", Subtraction)
+print("Multiplication: ", Multiplication)
+print("Division: ", Division)
---
+++ Task 2.py
@@ -0,0 +1,5 @@
+"""Create a Personalized Greeting"""
+First_Name = str(input("Enter your first name: "))
+last_Name = str(input("Enter your last name: "))
+Full_Name = First_Name +  " " + last_Name
+print("Hello,",Full_Name,"! " "Welcome to the Python program.")
